7fbea1f5592d70b0a77897325800ffe089658800
Abhishek Singh <abhisheksingh.vns0709@gmail.com>
2025-07-01 22:24:40 +0530

    showmore image  button add

--- index.js
+++ index.js
@@ -5,34 +5,35 @@ const input = document.querySelector(".search_box");
 const searchResult = document.querySelector("#search-result");
 const showmore = document.querySelector(".show-more-button");
 
-let inputData= "";
-let page = 1 ;
+let inputData = "";
+let page = 1;
 
-async function searchImage(){
-    inputData=input.value;
+async function searchImage() {
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apikey}`;
     const response = await fetch(url);
     const data = await response.json();
     const results = data.results;
 
-    results.map((result)=>{
+    results.map((result) => {
         const image = document.createElement("img");
         image.classList.add("image");
         image.src = result.urls.small;
-
         searchResult.appendChild(image);
-        
     });
 
+    // Show the button only if there are more pages
+    showmore.style.display = "block";
 }
 
-form.addEventListener("submit",(event) =>{
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     page = 1;
+    inputData = input.value;
+    searchResult.innerHTML = ""; // Only clear on a new search
     searchImage();
 });
 
-showmore.addEventListener("click",()=>{
+showmore.addEventListener("click", () => {
     page++;
     searchImage();
 });
